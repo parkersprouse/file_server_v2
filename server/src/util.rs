fec94168d5345e38bd189fd0d0ee72ef05c63b47
Parker Sprouse <parker@parkersprouse.me>
2025-11-25 00:04:04 -0600

    Handle decoding the paths on the server

--- server/src/util.rs
+++ server/src/util.rs
@@ -1,13 +1,23 @@
 use crate::AppState;
 use actix_web::{HttpRequest, HttpResponse, web::Data};
+use log::error;
 use std::{fs, io::Error as IOError, path::PathBuf};
+use urlencoding::decode;
 
 pub fn invalid_pathname(pathname: &str) -> bool {
   format!("/{pathname}/").contains("/../")
 }
 
 pub fn validate_path(req: &HttpRequest, data: &Data<AppState>) -> Result<PathBuf, HttpResponse> {
-  let pathname: &str = req.match_info().query("path").trim_matches('/');
+  let decoded_path = match decode(req.match_info().query("path")) {
+    Ok(path) => path.into_owned(),
+    Err(err) => {
+      error!("{err:?}");
+      return Err(HttpResponse::InternalServerError().finish())
+    },
+  };
+
+  let pathname: &str = decoded_path.trim_matches('/');
   if invalid_pathname(pathname) {
     return Err(HttpResponse::NotFound().finish());
   }
